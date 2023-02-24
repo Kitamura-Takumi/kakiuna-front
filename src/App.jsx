b9d9993594368afd918dd99df4204bacc342c3f4
Kitamura <takumi20030819@gmail.com>
2023-02-24 13:34:15 +0900

    react

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+const App = () => {
+  return (
+    <>
+  <div className="box">
+  <a  className="href" href="#">
+  <h1><span>札幌ドーム</span></h1>
+  <p>☆☆☆☆☆</p>
+  </a>
+</div>
+
+<div className="box">
+  <a  className="href" href="#">
+  <h1><span>札幌ドーム</span></h1>
+  <p>☆☆☆☆☆</p>
+  </a>
+</div>
+
+<div className="box">
+  <a  className="href" href="#">
+  <h1><span>札幌ドーム</span></h1>
+  <p>☆☆☆☆☆</p>
+  </a>
+</div>
+ </>
+)
+};
+
+export default App;
